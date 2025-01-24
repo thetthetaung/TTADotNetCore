18bba55837a11174e8eb7c5f42526c5f62da9ce6
thetthetaung <tta.ucsm@gmail.com>
2025-01-24 11:59:06 +0630

    feat: Charts

--- TTADotNetCore.ChartWebApp/Controllers/ApexChartController.cs
+++ TTADotNetCore.ChartWebApp/Controllers/ApexChartController.cs
@@ -44,5 +44,163 @@ namespace TTADotNetCore.ChartWebApp.Controllers
 
             return View(model);
         }
+
+        public IActionResult BarChart()
+        {
+            var model = new ApexChartBarChartModel
+            {
+                Series = new int[] { 400, 430, 448, 470, 540, 580, 690, 1100, 1200, 1380 },
+                Categories = new string[] { "South Korea", "Canada", "United Kingdom", "Netherlands", "Italy", "France", "Japan", "United States", "China", "Germany" }
+            };
+
+            return View(model);
+        }
+
+        public IActionResult BarWithGoals()
+        {
+            var model = new ApexChartBarWithGoalsModel
+            {
+                Series = new List<BarWithGoalsData>
+            {
+                new BarWithGoalsData
+                {
+                    Name = "Actual",
+                    Data = new List<BarDataPoint>
+                    {
+                        new BarDataPoint
+                        {
+                            X = "2011",
+                            Y = 12,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 14,
+                                    StrokeWidth = 2,
+                                    StrokeDashArray = new int[] { 2 },
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        },
+                        new BarDataPoint
+                        {
+                            X = "2012",
+                            Y = 44,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 54,
+                                    StrokeWidth = 5,
+                                    StrokeHeight = 10,
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        },
+                        new BarDataPoint
+                        {
+                            X = "2013",
+                            Y = 54,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 52,
+                                    StrokeWidth = 10,
+                                    StrokeHeight = 0,
+                                    StrokeLineCap = "round",
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        },
+                        new BarDataPoint
+                        {
+                            X = "2014",
+                            Y = 66,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 61,
+                                    StrokeWidth = 10,
+                                    StrokeHeight = 0,
+                                    StrokeLineCap = "round",
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        },
+                        new BarDataPoint
+                        {
+                            X = "2015",
+                            Y = 81,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 66,
+                                    StrokeWidth = 10,
+                                    StrokeHeight = 0,
+                                    StrokeLineCap = "round",
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        },
+                        new BarDataPoint
+                        {
+                            X = "2016",
+                            Y = 67,
+                            Goals = new List<Goal>
+                            {
+                                new Goal
+                                {
+                                    Name = "Expected",
+                                    Value = 70,
+                                    StrokeWidth = 5,
+                                    StrokeHeight = 10,
+                                    StrokeColor = "#775DD0"
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            };
+
+            return View(model);
+        }
+
+        public IActionResult DonutChart()
+        {
+            var model = new ApexChartDonutModel
+            {
+                Series = new int[] { 44, 55, 41, 17, 15 },
+                Labels = new string[] { "Apple", "Mango", "Orange", "Banana", "Grapes" }
+            };
+
+            return View(model);
+        }
+
+        public IActionResult RadarChart()
+        {
+            var model = new ApexChartRadarModel
+            {
+                Series = new List<RadarSeries>
+            {
+                new RadarSeries
+                {
+                    Name = "Series 1",
+                    Data = new int[] { 80, 50, 30, 40, 100, 20 }
+                }
+            },
+                Categories = new string[] { "January", "February", "March", "April", "May", "June" }
+            };
+
+            return View(model);
+        }
     }
 }
--- TTADotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
+++ TTADotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
@@ -19,4 +19,55 @@
         public string Type { get; set; }
         public int[] Data { get; set; }
     }
+
+    public class ApexChartBarChartModel
+    {
+        public int[] Series { get; set; }
+        public string[] Categories { get; set; }
+    }
+
+    public class ApexChartBarWithGoalsModel
+    {
+        public List<BarWithGoalsData> Series { get; set; }
+    }
+
+    public class BarWithGoalsData
+    {
+        public string Name { get; set; }
+        public List<BarDataPoint> Data { get; set; }
+    }
+
+    public class BarDataPoint
+    {
+        public string X { get; set; }
+        public int Y { get; set; }
+        public List<Goal> Goals { get; set; }
+    }
+
+    public class Goal
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+        public int StrokeWidth { get; set; }
+        public int? StrokeHeight { get; set; }
+        public int[] StrokeDashArray { get; set; }
+        public string StrokeColor { get; set; }
+        public string StrokeLineCap { get; set; }
+    }
+    public class ApexChartDonutModel
+    {
+        public int[] Series { get; set; }
+        public string[] Labels { get; set; }
+    }
+    public class ApexChartRadarModel
+    {
+        public List<RadarSeries> Series { get; set; }
+        public string[] Categories { get; set; }
+    }
+
+    public class RadarSeries
+    {
+        public string Name { get; set; }
+        public int[] Data { get; set; }
+    }
 }
